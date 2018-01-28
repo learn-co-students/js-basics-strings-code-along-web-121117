f84385a5d01639fbf0ea51c659f592a036c703c4
josephdlawson21 <josephdlawson21@gmail.com>
2018-01-28 15:48:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+
+
+let currentUser = "joseph";
+
+let welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = "Welcome, " + currentUser[0] + "!";
